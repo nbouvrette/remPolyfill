b46cf440da58bf38400b7ae054ce7ff26fe782e0
Nicolas Bouvrette <bouvrette.nicolas@gmail.com>
2017-06-08 23:20:31 -0400

    Update remPolyfill.js

--- remPolyfill.js
+++ remPolyfill.js
@@ -21,12 +21,71 @@ window.remPolyfill = {
     },
 
     /**
-     * Polyfill a link element's style.
+     * Get the body font size.
+     *
+     * @returns {Number} - The body font size in pixel (number only).
+     */
+    getBodyFontSize: function() {
+        if (!this.bodyFontSize) {
+            var hasNoBodyElement = false;
+
+            if (!document.body) {
+                hasNoBodyElement = true;
+                var bodyElement = document.createElement('body');
+                document.documentElement.appendChild(bodyElement);
+            }
+
+            this.bodyFontSize = parseFloat(this.getStyle(document.body, 'fontSize'));
+
+            if (hasNoBodyElement) {
+                document.documentElement.removeChild(bodyElement);
+                bodyElement = null;
+            }
+        }
+        return this.bodyFontSize;
+    },
+
+    /**
+     * Get the style of an element for a given property.
+     *
+     * @private
+     *
+     * @param {HTMLElement} element - The HTML element.
+     * @param {string} property     - The property of the style to get.
+     */
+    getStyle: function(element, property) {
+        if (typeof window.getComputedStyle !== 'undefined') {
+            return window.getComputedStyle(element, null).getPropertyValue(property);
+        } else {
+            return element.currentStyle[property];
+        }
+    },
+
+    /**
+     * Implement this script on a given element.
+     *
+     * @private
      *
      * @param {HTMLLinkElement} linkElement - The link element from which to get the CSS text.
+     * @param {string} cssText              - The CSS text of the link element.
      */
-    polyfill: function (linkElement) {
+    replaceRemWithPx: function (linkElement, cssText) {
+        if (cssText) {
+            // Replace all properties containing REM units with their pixel equivalents.
+            linkElement.styleSheet.cssText = cssText.replace(
+                /([\d]+\.[\d]+|\.[\d]+|[\d]+)rem/g, function (fullMatch, groupMatch) {
+                    return Math.round(parseFloat(groupMatch * remPolyfill.getBodyFontSize())) + 'px';
+                }
+            );
+        }
+    },
 
+    /**
+     * Implement this script on a given element.
+     *
+     * @param {HTMLLinkElement} linkElement - The link element to polyfill.
+     */
+    implement: function (linkElement) {
         if (!linkElement.href) {
             return;
         }
@@ -59,70 +118,6 @@ window.remPolyfill = {
         };
 
         request.send(null);
-    },
-
-    /**
-     * Get the body font size.
-     */
-    getBodyFontSize: function() {
-        if (!this.bodyFontSize) {
-            var hasNoBodyElement = false;
-
-            if (!document.body) {
-                hasNoBodyElement = true;
-                var bodyElement = document.createElement('body');
-                document.documentElement.appendChild(bodyElement);
-            }
-
-            this.bodyFontSize = parseFloat(this.getStyle(document.body, 'fontSize'));
-
-            if (hasNoBodyElement) {
-                document.documentElement.removeChild(bodyElement);
-                bodyElement = null;
-            }
-        }
-        return this.bodyFontSize;
-    },
-
-    /**
-     * Get the style of an element for a given property.
-     *
-     * @param {HTMLElement} element - The HTML element.
-     * @param {string} property     - The property of the style to get.
-     */
-    getStyle: function(element, property) {
-        if (typeof window.getComputedStyle !== 'undefined') {
-            return window.getComputedStyle(element, null).getPropertyValue(property);
-        } else {
-            return element.currentStyle[property];
-        }
-    },
-
-    /**
-     * Implement this script on a given element.
-     *
-     * @param {HTMLLinkElement} linkElement - The link element from which to get the CSS text.
-     * @param {string} cssText              - The CSS text of the link element.
-     */
-    replaceRemWithPx: function (linkElement, cssText) {
-        if (cssText) {
-            // Replace all properties containing REM units with their pixel equivalents.
-            linkElement.styleSheet.cssText = cssText.replace(
-                /([\d]+\.[\d]+|\.[\d]+|[\d]+)rem/g, function (fullMatch, groupMatch) {
-                    return Math.round(parseFloat(groupMatch * remPolyfill.getBodyFontSize())) + 'px';
-                });
-        }
-    },
-
-    /**
-     * Implement this script on a given element.
-     *
-     * @param {HTMLLinkElement} linkElement - The link element to polyfill.
-     */
-    implement: function (linkElement) {
-        if (linkElement.href) {
-            this.polyfill(linkElement);
-        }
     }
 };
 
